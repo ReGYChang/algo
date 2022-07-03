75062faee3f729f2ee9e854d00cabed4cb685f94
ReGYChang <p714140432@gmail.com>
2022-07-03 12:23:39 +0800

    algo: add 136 solution in go

--- 136/single_number.go
+++ 136/single_number.go
@@ -1,1 +1,10 @@
-package _36
+package main
+
+func singleNumber(nums []int) int {
+	n := nums[0]
+	l := len(nums)
+	for i := 1; i < l; i++ {
+		n = n ^ nums[i]
+	}
+	return n
+}
--- 136/single_number_test.go
+++ 136/single_number_test.go
@@ -1,1 +1,29 @@
 package main
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"single number", args{[]int{2, 2, 1}}, 1},
+		{"single number", args{[]int{4, 1, 2, 1, 2}}, 4},
+		{"single number", args{[]int{1}}, 1},
+		{"single number", args{[]int{3, 2, 3, 4, 4, 1, 2}}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+//Runtime: 16 ms, faster than 89.11% of Go online submissions for Single Number.
+//Memory Usage: 6.1 MB, less than 94.51% of Go online submissions for Single Number.
