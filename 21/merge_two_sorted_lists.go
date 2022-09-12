7537ae3da5d8fd1c5057bc4da3c4266011a52273
ReGYChang <p714140432@gmail.com>
2022-09-12 21:55:20 +0800

    algo: add 21 solution in Go without test

---
+++ 21/merge_two_sorted_lists.go
@@ -0,0 +1,27 @@
+package main
+
+// ListNode Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	if list1.Val >= list2.Val {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	} else {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	}
+}
+
+//Runtime: 0 ms, faster than 100.00% of Go online submissions for Merge Two Sorted Lists.
+//Memory Usage: 2.5 MB, less than 51.14% of Go online submissions for Merge Two Sorted Lists.
