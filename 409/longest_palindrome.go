7447889cfee2fa18566fe177d32ffac2ec8d58a6
ReGYChang <p714140432@gmail.com>
2022-09-15 23:02:55 +0800

    algo: add 409 solution in Go

---
+++ 409/longest_palindrome.go
@@ -0,0 +1,23 @@
+package main
+
+func longestPalindrome(s string) int {
+	m, l, res, oddExist := make(map[uint8]int), len(s), 0, false
+	for i := 0; i < l; i++ {
+		m[s[i]]++
+	}
+
+	for _, v := range m {
+		if v&1 == 0 {
+			res += v
+		} else {
+			res += v - 1
+			oddExist = true
+		}
+	}
+
+	if oddExist {
+		return res + 1
+	} else {
+		return res
+	}
+}
---
+++ 409/longest_palindrome_test.go
@@ -0,0 +1,53 @@
+package main
+
+import "testing"
+
+//Runtime: 3 ms, faster than 57.51% of Go online submissions for Longest Palindrome.
+//Memory Usage: 2.1 MB, less than 28.28% of Go online submissions for Longest Palindrome.
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "longest palindrome_1",
+			args: args{
+				s: "abccccdd",
+			},
+			want: 7,
+		},
+		{
+			name: "longest palindrome_2",
+			args: args{
+				s: "a",
+			},
+			want: 1,
+		},
+		{
+			name: "longest palindrome_3",
+			args: args{
+				s: "aaa",
+			},
+			want: 3,
+		},
+		{
+			name: "longest palindrome_3",
+			args: args{
+				s: "aaaccc",
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
