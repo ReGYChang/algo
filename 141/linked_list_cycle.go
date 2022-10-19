9f1f654e7d760502e3c0defce1996e9b26205459
ReGYChang <p714140432@gmail.com>
2022-10-19 22:38:15 +0800

    algo: add 141 solution in Go

---
+++ 141/linked_list_cycle.go
@@ -0,0 +1,37 @@
+package main
+
+// ListNode Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycleHashMap(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+
+	m := make(map[*ListNode]bool)
+	for head.Next != nil {
+		if _, exist := m[head]; !exist {
+			m[head] = true
+			head = head.Next
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
+func hasCycleTwoPointer(head *ListNode) bool {
+	var slow, fast = head, head
+	for fast != nil && fast.Next != nil {
+		if fast != slow {
+			fast = fast.Next.Next
+			slow = slow.Next
+		} else {
+			return true
+		}
+	}
+	return false
+}
---
+++ 141/linked_list_cycle_test.go
@@ -0,0 +1,120 @@
+package main
+
+import "testing"
+
+//Runtime: 18 ms, faster than 31.96% of Go online submissions for Linked List Cycle.
+//Memory Usage: 6.4 MB, less than 16.93% of Go online submissions for Linked List Cycle.
+
+func Test_hasCycleHashMap(t *testing.T) {
+	type args struct {
+		head []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "141-1",
+			args: args{
+				head: []int{3, 2, 0, -4},
+			},
+			want: false,
+		},
+		{
+			name: "141-2",
+			args: args{
+				head: []int{1, 2},
+			},
+			want: false,
+		},
+		{
+			name: "141-3",
+			args: args{
+				head: []int{1},
+			},
+			want: false,
+		},
+		{
+			name: "141-4",
+			args: args{
+				head: []int{},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			head := Ints2List(tt.args.head)
+			if got := hasCycleHashMap(head); got != tt.want {
+				t.Errorf("hasCycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_hasCycleTwoPointer(t *testing.T) {
+	type args struct {
+		head []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "141-1",
+			args: args{
+				head: []int{3, 2, 0, -4},
+			},
+			want: false,
+		},
+		{
+			name: "141-2",
+			args: args{
+				head: []int{1, 2},
+			},
+			want: false,
+		},
+		{
+			name: "141-3",
+			args: args{
+				head: []int{1},
+			},
+			want: false,
+		},
+		{
+			name: "141-4",
+			args: args{
+				head: []int{},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			head := Ints2List(tt.args.head)
+			if got := hasCycleTwoPointer(head); got != tt.want {
+				t.Errorf("hasCycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+//Runtime: 0 ms, faster than 100.00% of Go online submissions for Linked List Cycle.
+//Memory Usage: 4.3 MB, less than 100.00% of Go online submissions for Linked List Cycle.
+
+// Ints2List convert []int to List
+func Ints2List(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	l := &ListNode{}
+	t := l
+	for _, v := range nums {
+		t.Next = &ListNode{Val: v}
+		t = t.Next
+	}
+	return l.Next
+}
