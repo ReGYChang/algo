6e7476bf8feb1c67b5e6205776817b8a7c082253
ReGYChang <p714140432@gmail.com>
2022-10-22 23:27:42 +0800

    algo: add 53 solution in Go

---
+++ 53/maximum_subarray.go
@@ -0,0 +1,18 @@
+package main
+
+func maxSubArray(nums []int) int {
+	dp := make([]int, len(nums), len(nums))
+	dp[0] = nums[0]
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = dp[i-1] + nums[i]
+		} else {
+			dp[i] = nums[i]
+		}
+		if dp[i] > max {
+			max = dp[i]
+		}
+	}
+	return max
+}
--- 53/maximum_subarray_test.go
+++ 53/maximum_subarray_test.go
@@ -2,6 +2,9 @@ package main
 
 import "testing"
 
+//Runtime: 169 ms, faster than 64.52% of Go online submissions for Maximum Subarray.
+//Memory Usage: 9.4 MB, less than 58.13% of Go online submissions for Maximum Subarray.
+
 func Test_maxSubArray(t *testing.T) {
 	type args struct {
 		nums []int
@@ -11,10 +14,35 @@ func Test_maxSubArray(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "53-1",
+			args: args{nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}},
+			want: 6,
+		},
+		{
+			name: "53-2",
+			args: args{nums: []int{1}},
+			want: 1,
+		},
+		{
+			name: "53-3",
+			args: args{nums: []int{5, 4, -1, 7, 8}},
+			want: 23,
+		},
+		{
+			name: "53-4",
+			args: args{nums: []int{-1, -2}},
+			want: -1,
+		},
+		{
+			name: "53-5",
+			args: args{nums: []int{1, 2}},
+			want: 3,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+
 			if got := maxSubArray(tt.args.nums); got != tt.want {
 				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
 			}
